e2d58aa0754da8fee7dba4d76e4ecbb03139bca9
Timo Nicolai <timonicolai@arcor.de>
2021-10-30 20:31:36 +0200

    Emit encoding code

--- AutoYaml.cpp
+++ AutoYaml.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <string>
 
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
@@ -25,21 +26,105 @@ static llvm::cl::OptionCategory AutoYAMLToolCategory { "autoyaml options" };
 
 static llvm::cl::extrahelp CommonHelp { clang::tooling::CommonOptionsParser::HelpMessage };
 
+// Convenience wrapper around llvm::raw_fd_ostream that handles indentation levels nicely.
+class AutoYAMLOS
+{
+  template<typename T>
+  friend AutoYAMLOS &operator<<(AutoYAMLOS &, T const &);
+
+  enum { TABSTOP = 2 };
+
+  struct EndLine {};
+  struct EndBlock {};
+
+public:
+  static inline EndLine const EndL; // End of line.
+  static inline EndBlock const EndB; // End of block.
+
+  AutoYAMLOS(const std::string &File)
+  {
+    std::error_code EC;
+    OS_ = std::make_unique<llvm::raw_fd_ostream>(File, EC);
+    OSValid_ = !EC;
+  }
+
+  operator bool() const
+  { return OSValid_; }
+
+  // Increase indentation level.
+  void incIndLvl()
+  { ++IndLvl_; }
+
+  // Decrease indentation level.
+  void decIndLvl()
+  {
+    assert(IndLvl_ > 0);
+    --IndLvl_;
+  }
+
+private:
+  std::unique_ptr<llvm::raw_fd_ostream> OS_;
+  bool OSValid_ {false};
+
+  unsigned Ind_ {0};
+  unsigned IndLvl_ {0};
+  bool IndActive_ {true};
+};
+
+template<typename T>
+AutoYAMLOS &operator<<(AutoYAMLOS &OS, T const &Val)
+{
+  assert(OS);
+
+  if (OS.IndActive_) {
+    for (unsigned i = 0; i < AutoYAMLOS::TABSTOP * OS.IndLvl_; ++i)
+      *OS.OS_ << ' ';
+
+    OS.IndActive_ = false;
+  }
+
+  *OS.OS_ << Val;
+
+  return OS;
+}
+
+template<>
+AutoYAMLOS &operator<<(AutoYAMLOS &OS, AutoYAMLOS::EndLine const &)
+{
+  assert(OS);
+
+  *OS.OS_ << '\n';
+  OS.IndActive_ = true;
+
+  return OS;
+}
+
+template<>
+AutoYAMLOS &operator<<(AutoYAMLOS &OS, AutoYAMLOS::EndBlock const &)
+{
+  assert(OS);
+
+  *OS.OS_ << "\n\n";
+  OS.IndActive_ = true;
+
+  return OS;
+}
+
 class AutoYAMLMatchCallback : public clang::ast_matchers::MatchFinder::MatchCallback
 {
 public:
-  AutoYAMLMatchCallback(llvm::raw_fd_ostream &OutStream,
-                        clang::ASTContext &Context)
-  : OutStream_(OutStream),
-    Context_(Context)
+  AutoYAMLMatchCallback(AutoYAMLOS &OS)
+  : OS_(OS)
   {}
 
   void run(clang::ast_matchers::MatchFinder::MatchResult const &Result) override
   {
-    auto Record { Result.Nodes.getNodeAs<clang::RecordDecl>("AutoYAML") };
+    // Get record declaration node.
+    auto Record { Result.Nodes.getNodeAs<clang::CXXRecordDecl>("AutoYAML") };
     assert(Record);
     assert(Record->hasAttrs());
 
+    // Check if it's annotated with __attribute__((annotate("AutoYAML"))).
     auto Attr { Record->getAttrs()[0] };
     auto AnnotateAttr { llvm::dyn_cast<clang::AnnotateAttr>(Attr) };
     assert(AnnotateAttr);
@@ -47,37 +132,108 @@ public:
     if (AnnotateAttr->getAnnotation() != "AutoYAML")
       return;
 
-    OutStream_ << Record->getName() << '\n';
+    // Emit conversion code.
+    emitConvert(Record);
   }
 
 private:
-  llvm::raw_fd_ostream &OutStream_;
-  clang::ASTContext &Context_;
+  void emitConvert(const clang::CXXRecordDecl *Record)
+  {
+    auto RecordName { Record->getName() };
+
+    OS_ << "template<> struct convert<" << RecordName << "> {" << OS_.EndL;
+
+    OS_.incIndLvl();
+
+    emitEncode(Record);
+
+    emitDecode(Record);
+
+    OS_.decIndLvl();
+
+    OS_ << "};" << OS_.EndB;
+  }
+
+  void emitEncode(const clang::CXXRecordDecl *Record)
+  {
+    auto RecordName { Record->getName() };
+
+    OS_ << "static Node encode(const " << RecordName << " &obj) {" << OS_.EndL;
+
+    OS_.incIndLvl();
+
+    OS_ << "Node node;" << OS_.EndL;
+
+    for (auto Field : Record->fields()) {
+      // Skip non-public members.
+      if (Field->getAccess() != clang::AS_public)
+        continue;
+
+      OS_ << "node.push_back(obj." << Field->getNameAsString() << ");" << OS_.EndL;
+    }
+
+    OS_ << "return node;" << OS_.EndL;
+
+    OS_.decIndLvl();
+
+    OS_ << "}" << OS_.EndB;
+  }
+
+  void emitDecode(const clang::CXXRecordDecl *Record)
+  {
+    // XXX
+  }
+
+  AutoYAMLOS &OS_;
 };
 
 class AutoYAMLASTConsumer : public clang::ASTConsumer
 {
 public:
-  AutoYAMLASTConsumer(llvm::raw_fd_ostream &OutStream)
-  : OutStream_(OutStream)
+  AutoYAMLASTConsumer(AutoYAMLOS &OS)
+  : OS_(OS)
   {}
 
   void HandleTranslationUnit(clang::ASTContext &Context) override
   {
     using namespace clang::ast_matchers;
 
+    // Create AST Matcher.
     auto AutoYAMLMatchExpression { recordDecl(hasAttr(clang::attr::Annotate)) };
 
-    AutoYAMLMatchCallback MatchCallback { OutStream_, Context };
+    AutoYAMLMatchCallback MatchCallback { OS_ };
 
     clang::ast_matchers::MatchFinder MatchFinder;
     MatchFinder.addMatcher(AutoYAMLMatchExpression.bind("AutoYAML"), &MatchCallback);
 
+    // Emit conversion code.
+    emitPreamble();
+
+    OS_.incIndLvl();
+
     MatchFinder.matchAST(Context);
+
+    OS_.decIndLvl();
+
+    emitEpilogue();
   }
 
 private:
-  llvm::raw_fd_ostream &OutStream_;
+  void emitPreamble()
+  {
+    OS_ << "// Automatically generated by AutoYAML, do not modify!" << OS_.EndB;
+
+    OS_ << "#pragma once" << OS_.EndB;
+
+    OS_ << "namespace YAML {" << OS_.EndB;
+  }
+
+  void emitEpilogue()
+  {
+    OS_ << "} // end namespace YAML";
+  }
+
+  AutoYAMLOS &OS_;
 };
 
 struct AutoYAMLFrontendAction : public clang::ASTFrontendAction
@@ -85,23 +241,24 @@ struct AutoYAMLFrontendAction : public clang::ASTFrontendAction
   std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance&,
                                                         llvm::StringRef File) override
   {
+    // Create output file stream.
     std::string InFileStem { llvm::sys::path::stem(File).str() };
 
     std::string OutFile { InFileStem + ".AutoYAML.h" };
 
-    std::error_code EC;
-    OutStream_ = std::make_unique<llvm::raw_fd_ostream>(OutFile, EC);
+    OS_ = std::make_unique<AutoYAMLOS>(OutFile);
 
-    if (EC) {
+    if (!*OS_) {
       llvm::errs() << "Failed to create output file \"" << OutFile << "\"\n";
       return nullptr;
     }
 
-    return std::make_unique<AutoYAMLASTConsumer>(*OutStream_);
+    // Run AST consumer.
+    return std::make_unique<AutoYAMLASTConsumer>(*OS_);
   }
 
 private:
-  std::unique_ptr<llvm::raw_fd_ostream> OutStream_;
+  std::unique_ptr<AutoYAMLOS> OS_;
 };
 
 int main(int argc, char const **argv)
